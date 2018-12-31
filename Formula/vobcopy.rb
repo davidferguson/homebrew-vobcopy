6f5c524f4efbad66d6931a63a093ced85d4f5107
David Ferguson <davidferguson@users.noreply.github.com>
2018-12-31 19:06:03 +0000

    Update vobcopy.rb

--- Formula/vobcopy.rb
+++ Formula/vobcopy.rb
@@ -1,12 +1,11 @@
-require 'formula'
-
 class Vobcopy < Formula
-  url 'http://web.archive.org/web/20160309064600/http://vobcopy.org/download/vobcopy-1.2.0.tar.bz2'
-  homepage 'http://vobcopy.org'
-  md5 '88f735ccd051093ff40dab4597bc586e'
+  desc "Copy DVD .vob files to hard disk"
+  homepage "http://vobcopy.org"
+  url "http://web.archive.org/web/20160309064600/http://vobcopy.org/download/vobcopy-1.2.0.tar.bz2"
+  sha256 "892504d195d06a80ab283db642eb2ccbf9f1dc6ba5ff0fdfcf7a9cb660f48106"
 
-  depends_on 'libdvdread'
-  depends_on 'libdvdcss'
+  depends_on "libdvdread"
+  depends_on "libdvdcss"
 
   def install
     system "./configure.sh", "--prefix=#{prefix}",
@@ -14,6 +13,6 @@ class Vobcopy < Formula
                              "--with-lfs",
                              "--with-dvdread-libs=#{HOMEBREW_PREFIX}"
     system "make"
-    system "make install"
+    system "make", "install"
   end
 end
